2748d77175447d6915faf4cf3669e47dbf8b9443
Eddi De Pieri <eddi@depieri.net>
2015-03-27 15:00:32 +0100

    fix curl - test

--- build.sh
+++ build.sh
@@ -231,7 +231,9 @@ fi
 mkdir -p .repo/local_manifests
 rm -f .repo/local_manifest.xml
 
+set -x
 curl https://raw.githubusercontent.com/KFireHDX/android_device_amazon_hdx-common/$REPO_BRANCH/hdx-common.xml > .repo/local_manifests/hdx-common.xml
+set +x
 
 echo Core Manifest:
 cat .repo/manifest.xml
